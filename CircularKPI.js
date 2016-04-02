5e4a011cd26a137752aba3dd343c1f9c8f7384d1
johsund <jsn@qlik.com>
2016-04-02 17:00:06 +0800

    Final fix

--- CircularKPI.js
+++ CircularKPI.js
@@ -63,6 +63,16 @@ define(["jquery", "text!./scripts/style.css", "./scripts/themes", "./scripts/d3.
         paint: function($element, layout) {
 
             $element.empty();
+			
+			var id = "container_" + layout.qInfo.qId;
+			var width = $element.width();
+			var height = $element.height();
+			if(document.getElementById(id)) {
+				$("#" + id).empty();
+			}
+			else {
+				$element.append($('<div />').attr("id", id).attr("class", "viz").width(width).height(height));
+			}
 
             var IS_SPARK_LAYOUT = this.options.layoutMode === 1;
             var HAS_DIMENSION = layout.qHyperCube.qDimensionInfo.length > 0;
@@ -70,11 +80,9 @@ define(["jquery", "text!./scripts/style.css", "./scripts/themes", "./scripts/d3.
             var data = layout.qHyperCube.qDataPages[0].qMatrix;
             var label = layout.qHyperCube.qMeasureInfo[0].qFallbackTitle;
 
-            var colors = [layout.theme[0], layout.theme[1]];
+            var colors = [layout.theme[0], layout.theme[1], layout.theme[2]];
             var animationTime = layout.animationtime;
 
-            var width = $element.width();
-            var height = $element.height();
 
             var rows, columns;
             if (height > width) {
@@ -85,14 +93,14 @@ define(["jquery", "text!./scripts/style.css", "./scripts/themes", "./scripts/d3.
                 rows = Math.ceil(data.length / columns);
             };
 
-            var area = d3.select($element.get(0))
+            var area = d3.select($("#" + id).get(0))
                 .selectAll('.area')
                 .data(data)
                 .enter()
                 .append('div')
                 .attr('class', 'circular-kpi-tile')
                 .attr('id', function(d, i) {
-                    return 'circular-kpi-tile-' + i;
+                    return id + '_circular-kpi-tile-' + i;
                 })
                 .style('width', Math.ceil(width / columns, 10) - 5 + 'px')
                 .style('height', Math.ceil(height / rows, 10) - 5 + 'px');
@@ -101,9 +109,10 @@ define(["jquery", "text!./scripts/style.css", "./scripts/themes", "./scripts/d3.
                 var value = HAS_DIMENSION ? d[1].qNum : d[0].qNum;
                 var label = HAS_DIMENSION ? d[0].qText : '';
 
-                var element = document.getElementById('circular-kpi-tile-' + i);
+                var element = document.getElementById(id + '_circular-kpi-tile-' + i);
+				
                 var width = element.offsetWidth - 5, height = element.offsetHeight - 5;
-                
+				
                 var select = function() {
                     return HAS_DIMENSION ? this.selectValues(0, [d[0].qElemNumber], true) : false;
                 }.bind(this);
@@ -131,3 +140,9 @@ define(["jquery", "text!./scripts/style.css", "./scripts/themes", "./scripts/d3.
         }
     };
 });
+
+//function GetElementInsideContainer(containerID, childID) {
+//    var elm = document.getElementById(childID);
+//    var parent = elm ? elm.parentNode : {};
+ //   return (parent.id && parent.id === containerID) ? elm : {};
+//}
